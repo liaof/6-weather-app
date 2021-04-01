679af5ad8e63473e2bc63829f4ade7083b77a734
liaof <f.liao163@gmail.com>
2021-03-31 22:19:17 -0400

    fixed icon cloning issue

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,45 +1,90 @@
 var apiKey = "f25b698e5aca7ef3e7493ebf59f1c37d";
+var tasks = {};
+var dayCards = [$("day1"),$("day2"),$("day3"),$("day4"),$("day5")];
+var loadTasks = function() {
+    if (!tasks) {
+        tasks = new [];
+    };
+}
 
-
+var saveTasks = function(){
+    console.log("saved");
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 var generateCurrentInfo = function(data){
     var temp = data.current.feels_like;
     var humi = data.current.humidity;
     var wind = data.current.wind_speed;
     var uv = data.current.uvi;
     
+    
     $("#tempDisplay").html("Temperature: "+temp+"℃");
     $("#humiDisplay").html("Humidity: "+humi+"%");
     $("#windDisplay").html("Wind Speed: "+wind+"km/h");
     $("#uvDisplay").html("UV Index: "+uv).addClass("rounded");
-    if (uv<=3){
-        $("#uvDisplay").addClass("text-success"); 
+   
+
+    if (uv>=7){//high UV index
+        $("#uvDisplay").addClass("text-danger"); 
     } else if (uv>3&&u<7){
         $("#uvDisplay").addClass("text-warning"); 
-    } else if (uv<=7){
-        $("#uvDisplay").addClass("text-danger"); 
-    }
+    } else if (uv<=3){//low UV index
+        $("#uvDisplay").addClass("text-success"); 
+    };
    
 };
 var generateForecastInfo = function(data){
-    console.log(data);
-    console.log(data.daily[0]);
+  
     var dayCard = [];
     for (var i = 0; i<5;i++){
         dayCard[i] = $(".forecastCard[index="+i+"]");
-        
-        console.log(dayCard[i][0]);
-        console.log(data)
+        console.log(i);
+
         var temp = data.daily[i].feels_like.day;
         var humi = data.daily[i].humidity;
         var icon =  data.daily[i].weather[0].icon;
         var date = moment().add(i, 'days').format("MMM Do");
 
-       
+      
 
         
         var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-        var ikon = $("<img>").attr("id", "wicon").attr("src", iconurl).attr("alt", "Weather Icon");
-        $(dayCard[i][0]).children("#icon").append(ikon);
+       console.log(iconurl);
+        //if (ikon ===undefined){
+        //    var ikon = $("<img>").attr("id", "wicon").attr("src", iconurl).attr("alt", "Weather Icon");
+        //} else {
+        //    $( "ikon" ).remove();
+        //};
+        
+        
+        
+            //$("ikon").remove();
+            //console.log("remoe");
+            //var ikon = $("<img>").attr("id", "wicon").attr("src", iconurl).attr("alt", "Weather Icon");
+            //$("#iconimg").attr("id","wicon").add("src", iconurl);
+        $("#iconimg").attr("src", iconurl);
+        console.log($("#iconimg"));
+        //var child = $((dayCard[i].children())[1]);
+        //console.log($(child).html());
+        console.log($(dayCard[i].children().val));
+        console.log($(dayCard[i][0]).children[1]);
+        var currentIconSpaceEl = $(dayCard[i].children("#icon").children("#iconimg"));
+        $(currentIconSpaceEl).attr("src", iconurl);
+        //$(dayCard[i][0]).children("#icon").append(ikon);
+        //console.log(ikon);
+        //console.log(ikon.siblings());
+        //var iconSibling = ikon.siblings();
+       // console.log(iconSibling);
+        //console.log(iconSibling.length);
+       //var previous = iconSibling.prev().has("id","wicon");
+        //console.log(previous);
+       //if (!$(iconSibling).length==0){
+          //  $(this).remove(previous);
+          //  console.log("has siblings");
+       // }; 
+        
+        
+        //$(dayCard[i][0]).children(".iconplaceholder").replaceWith(ikon);
         //console.log(iconurl);
         //console.log(icon);
         //console.log($(dayCard[i][0]).children("#icon").children("#wicon"));
@@ -52,7 +97,7 @@ var generateForecastInfo = function(data){
     }
 }
 var getWeatherRepo = function(lat, lon){
-    console.log(lat, lon);
+    
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&units=metric&appid="+apiKey;
     fetch(apiUrl).then(function(response){
         if(response.ok){
@@ -75,10 +120,10 @@ var getCoordinates = function(thisCity){
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+thisCity+"&limit=1&appid="+apiKey;
     fetch(apiUrl).then(function(response){
         if(response.ok){
-            console.log(response);
+
             response.json().then(function(data){
                 if(data.length!=0){
-                    console.log(data);
+                    
                     //set our card title/name here so you can enter co-ordinates when searching for a city and still come up with the name
                     $("#currentCityDate").html(data[0].name+" "+"");
                     getWeatherRepo(data[0].lat, data[0].lon);
@@ -95,10 +140,10 @@ var getCoordinates = function(thisCity){
 
 
 
-
-
 $(".city-form").on("click",".btn", function(event){
     event.preventDefault();
     var thisCity = $("#thisCity").val();
+    
     getCoordinates(thisCity);
+    saveTasks();
 });
