7256d86ab68e26f78bcbbe59a6ad3ed0f282db89
liaof <f.liao163@gmail.com>
2021-04-02 02:50:05 -0400

    save as object

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,29 +1,46 @@
 var apiKey = "f25b698e5aca7ef3e7493ebf59f1c37d";
-var tasks = {};
+var cities = {};
 var dayCards = [$("day1"),$("day2"),$("day3"),$("day4"),$("day5")];
-var loadTasks = function() {
-    if (!tasks) {
-        tasks = new [];
-    };
-}
 
-var saveTasks = function(){
+var createHistoryLinks = function(){
+
+    //
+//$("searchHistory").append("cityLink");
+};
+var loadCities = function() {
+    cities = JSON.parse(localStorage.getItem("cities"));
+    
+    //loop over each cities value
+    if (!cities){
+        cities = {lat: "", lon: ""};
+
+    }
+    //loop over each cities object
+    $.each(cities, function(){
+        createHistorLinks(cities.lat, cities.lon);
+    });
+};
+
+var saveCities = function(thisLat,thisLon){
+    cities.lat=thisLat;
+    cities.lon=thisLon;
     console.log("saved");
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
+//fill out the top right quad of the site
 var generateCurrentInfo = function(data){
     var temp = data.current.feels_like;
     var humi = data.current.humidity;
     var wind = data.current.wind_speed;
     var uv = data.current.uvi;
     
-    
+
     $("#tempDisplay").html("Temperature: "+temp+"℃");
     $("#humiDisplay").html("Humidity: "+humi+"%");
     $("#windDisplay").html("Wind Speed: "+wind+"km/h");
     $("#uvDisplay").html("UV Index: "+uv).addClass("rounded");
    
-
+    //UV index coloring thing
     if (uv>=7){//high UV index
         $("#uvDisplay").addClass("text-danger"); 
     } else if (uv>3&&u<7){
@@ -33,69 +50,41 @@ var generateCurrentInfo = function(data){
     };
    
 };
+
+//fill out the 2nd quadrant of the side, each day individually and consecutively
 var generateForecastInfo = function(data){
   
-    var dayCard = [];
+    var dayCardEl = [];
     for (var i = 0; i<5;i++){
-        dayCard[i] = $(".forecastCard[index="+i+"]");
-        console.log(i);
+        dayCardEl[i] = $(".forecastCard[index="+i+"]");
 
         var temp = data.daily[i].feels_like.day;
         var humi = data.daily[i].humidity;
         var icon =  data.daily[i].weather[0].icon;
-        var date = moment().add(i, 'days').format("MMM Do");
+        var date = moment().add(i+1, 'days').format("MMM Do");
 
-      
-
-        
         var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
-       console.log(iconurl);
-        //if (ikon ===undefined){
-        //    var ikon = $("<img>").attr("id", "wicon").attr("src", iconurl).attr("alt", "Weather Icon");
-        //} else {
-        //    $( "ikon" ).remove();
-        //};
-        
-        
-        
-            //$("ikon").remove();
-            //console.log("remoe");
-            //var ikon = $("<img>").attr("id", "wicon").attr("src", iconurl).attr("alt", "Weather Icon");
-            //$("#iconimg").attr("id","wicon").add("src", iconurl);
+       //console.log(iconurl);
+
         $("#iconimg").attr("src", iconurl);
-        console.log($("#iconimg"));
-        //var child = $((dayCard[i].children())[1]);
-        //console.log($(child).html());
-        console.log($(dayCard[i].children().val));
-        console.log($(dayCard[i][0]).children[1]);
-        var currentIconSpaceEl = $(dayCard[i].children("#icon").children("#iconimg"));
+        //console.log($("#iconimg"));
+
+        //console.log($(dayCardEl[i].children().val));
+        //console.log($(dayCardEl[i][0]).children[1]);
+
+        //finds the <img> element of the currently indexed dayCard
+        var currentIconSpaceEl = $(dayCardEl[i].children("#icon").children("#iconimg"));
+        //setIcon
         $(currentIconSpaceEl).attr("src", iconurl);
-        //$(dayCard[i][0]).children("#icon").append(ikon);
-        //console.log(ikon);
-        //console.log(ikon.siblings());
-        //var iconSibling = ikon.siblings();
-       // console.log(iconSibling);
-        //console.log(iconSibling.length);
-       //var previous = iconSibling.prev().has("id","wicon");
-        //console.log(previous);
-       //if (!$(iconSibling).length==0){
-          //  $(this).remove(previous);
-          //  console.log("has siblings");
-       // }; 
+    
+        //set the values of the currently indexed dayCard
+        $(dayCardEl[i][0]).children("#forecastDate").html(date);
         
-        
-        //$(dayCard[i][0]).children(".iconplaceholder").replaceWith(ikon);
-        //console.log(iconurl);
-        //console.log(icon);
-        //console.log($(dayCard[i][0]).children("#icon").children("#wicon"));
-        $(dayCard[i][0]).children("#forecastDate").html(date);
-        
-        $(dayCard[i][0]).children("#forecastTempDisplay").html("Temp: "+temp+"℃");
-        $(dayCard[i][0]).children("#forecastHumiDisplay").html("Humidity: "+humi+"%");
-        //$(dayCard[i]).children[3].html(humi);
-        //console.log($(dayCard[0]).children("#forecastTempValue").val)
+        $(dayCardEl[i][0]).children("#forecastTempDisplay").html("Temp: "+temp+"℃");
+        $(dayCardEl[i][0]).children("#forecastHumiDisplay").html("Humidity: "+humi+"%");
     }
-}
+};
+
 var getWeatherRepo = function(lat, lon){
     
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&units=metric&appid="+apiKey;
@@ -109,12 +98,9 @@ var getWeatherRepo = function(lat, lon){
             })
         }
     })
-}
+};
 
-
-
-
-
+//the OWP api only takes co-ordinates as input. Luckily they have another api we can use to convert city names to their co-ordinates
 var getCoordinates = function(thisCity){
     //use the OpenWeatherMap api to find the co-ordinates of our city
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q="+thisCity+"&limit=1&appid="+apiKey;
@@ -122,28 +108,32 @@ var getCoordinates = function(thisCity){
         if(response.ok){
 
             response.json().then(function(data){
-                if(data.length!=0){
+                if(data.length!=0){//check if the data actually exists
                     
-                    //set our card title/name here so you can enter co-ordinates when searching for a city and still come up with the name
-                    $("#currentCityDate").html(data[0].name+" "+"");
+                    //update card title with city name and current date
+                    $("#currentCityDate").html(data[0].name+" "+moment().format("MMM Do"));
                     getWeatherRepo(data[0].lat, data[0].lon);
-                } else {
+                    
+                    saveCities(data[0].lat, data[0].lon);
+                } else {//catch successfully fetched data that does not have the information we need
                     document.location.replace("./index.html");
                 }
             })
-        } else {
+        } else {//only catches extremely wrong inputs, like single letters and special characters. need another error handler inside
             document.location.replace("./index.html");
         }
-        
     })
 };
-
-
 
 $(".city-form").on("click",".btn", function(event){
     event.preventDefault();
     var thisCity = $("#thisCity").val();
     
     getCoordinates(thisCity);
-    saveTasks();
+    console.log("finished");
+    cities.push(thisCity);
+    saveCities();
+    console.log(cities);
 });
+
+//loadCities();
